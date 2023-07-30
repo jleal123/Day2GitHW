9bd4a7e89a85a817a543abaf8ab58c0c21795505
Joshua Leal <Joshua Leal@JoshPC>
2023-07-29 20:28:30 -0400

    Initiated Demo Class

---
+++ src/GitHW/DemoGitHW.java
@@ -0,0 +1,18 @@
+package GitHW;
+
+public class DemoGitHW {
+
+	public static void main(String[] args) {
+
+		System.out.println("Joshua Leal");
+
+	}
+
+	public static void hey() {
+		
+		System.out.println("Hey");
+	}
+	
+	
+	
+}
